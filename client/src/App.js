9a2e47844854778b618c57d4a0e87c0902d4b5b2
Amantha Jayathilake <amanthajayathilake@gmail.com>
2021-11-14 23:21:07 +0530

    Creating advertisement routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,16 @@ import React from 'react'
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Home from "./pages/Home";
-
+import CreatePost from "./pages/CreatePost";
 function App() {
   
   return (
     <div className="App">
       <Router>
+        <Link to="/createpost">Create a Post</Link>
         <Switch>
           <Route path="/" exact component={Home}/>
+          <Route path="/createpost" exact component={CreatePost}/>
         </Switch>
       </Router>
     </div>
