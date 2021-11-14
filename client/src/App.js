6daea4556307a99a3d767ba93a82d4acda2bf2f3
Amantha Jayathilake <amanthajayathilake@gmail.com>
2021-11-14 16:01:24 +0530

    FRONTEND - Advertisement design

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,28 @@
-import './App.css';
+import "./App.css";
 import axios from "axios";
-import {useEffect,useState} from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-
   const [listOfPosts, setListOfPosts] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3001/posts").then((response)=>{
+    axios.get("http://localhost:3001/posts").then((response) => {
       setListOfPosts(response.data);
     });
   }, []);
-
-  return <div className="App">
-    {listOfPosts.map((value,key) => {
-      return (
-      <div classname="post">
-        <div className="post-title"> {value.title}  </div>
-        <div className="post-body"> {value.postText} </div>
-        <div className="post-footer"> {value.username} </div>
-      </div>);
-    })}
-  </div>
+  return (
+    <div className="App">
+      {listOfPosts.map((value, key) => {
+        return (
+          <div className="post">
+            <div className="title"> {value.title} </div>
+            <div className="body">{value.postText}</div>
+            <div className="footer">{value.username}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
